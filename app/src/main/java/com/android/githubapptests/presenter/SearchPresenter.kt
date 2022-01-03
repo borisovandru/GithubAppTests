6acbfde090df24a99d7b80e521f8079392c65d8b
Borisov Andrey <80087707+borisovandru@users.noreply.github.com>
2022-01-03 21:13:00 +0400

    HomeWork for lesson 2

---
+++ app/src/main/java/com/android/githubapptests/presenter/SearchPresenter.kt
@@ -0,0 +1,41 @@
+package com.android.githubapptests.presenter
+
+import com.android.githubapptests.model.SearchResponse
+import com.android.githubapptests.repository.GitHubRepository
+import com.android.githubapptests.view.ViewContract
+import retrofit2.Response
+
+internal class SearchPresenter internal constructor(
+    private val viewContract: ViewContract,
+    private val repository: GitHubRepository
+) : PresenterContract, GitHubRepository.GitHubRepositoryCallback {
+
+    override fun searchGitHub(searchQuery: String) {
+        viewContract.displayLoading(true)
+        repository.searchGithub(searchQuery, this)
+    }
+
+    override fun handleGitHubResponse(response: Response<SearchResponse?>?) {
+        viewContract.displayLoading(false)
+        if (response != null && response.isSuccessful) {
+            val searchResponse = response.body()
+            val searchResults = searchResponse?.searchResults
+            val totalCount = searchResponse?.totalCount
+            if (searchResults != null && totalCount != null) {
+                viewContract.displaySearchResults(
+                    searchResults,
+                    totalCount
+                )
+            } else {
+                viewContract.displayError("Search results or total count are null")
+            }
+        } else {
+            viewContract.displayError("Response is null or unsuccessful")
+        }
+    }
+
+    override fun handleGitHubError() {
+        viewContract.displayLoading(false)
+        viewContract.displayError()
+    }
+}
